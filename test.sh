be26aaaee1eb59339462ecbfe7779fbeec7c18f0
Yule Fox <yulefox@gmail.com>
2017-03-18 18:27:40 +0800

    modify test

--- test.sh
+++ test.sh
@@ -6,8 +6,7 @@ export LAMP_CONFIG_PATH=$PWD/contrib
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor) ; do
-    #go test -v -race -coverprofile=profile.out -covermode=atomic -timeout 90s $d
-    go test -v -race -timeout 90s $d
+    go test -v -race -coverprofile=profile.out -covermode=atomic -timeout 90s $d
     if [ -f profile.out ] ; then
         cat profile.out >> coverage.txt
         rm profile.out
